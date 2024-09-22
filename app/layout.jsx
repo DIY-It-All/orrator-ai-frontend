3870b8a872b65be310a89730fadc23d512389520
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-09-22 23:28:33 +0530

    somechanges?

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
-import { Footer } from "./Footer.js";
+import { Footer } from "./Footer";
 import "./globals.css";
-import { NavBarLM } from "./NavBarLM.js";
-
+import { NavBarLM } from "./NavBarLM";
+import { MetaMaskProvider } from "@metamask/sdk-react"
 
 export const metadata = {
   title: "Orrator-AI",
@@ -11,14 +11,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Oracle AI",
+          url: window.location.href,
+        },
+        infuraAPIKey: process.env.INFURA_API_KEY,
+        // Other options.
+      }}
+    >
       <body
       //defult dark theme
         className="bg-gray-900 text-white"
       >
-        <NavBarLM></NavBarLM>
+        <NavBarLM />
         {children}
         <Footer />
       </body>
+      </MetaMaskProvider>
     </html>
   );
 }
