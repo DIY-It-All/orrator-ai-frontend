5fb417b50c2c781835e63a08cea8359c86cd8872
DIY-It-All <138210699+DIY-It-All@users.noreply.github.com>
2024-09-30 21:34:48 +0530

    images updated

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -80,7 +80,7 @@ export function NavBar() {
     <nav className="bg-white border-gray-200 py-2.5 text-black pt-[1em]">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                 <a href="./" className="flex items-center">
-                    <img src="/v1.svg" className="h-6 mr-3 sm:h-9" alt="Orator-AI Logo" />
+                    <img src="/logo.png" className="h-6 mr-3 sm:h-9" alt="Orator-AI Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap ">Orator-AI</span>
                 </a>
                 <div className="flex items-center lg:order-2">
