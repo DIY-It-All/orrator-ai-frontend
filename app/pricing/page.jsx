20711e70e099723a428c99b6487ce441606a0bdd
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-10-06 23:37:15 +0530

    Update page.jsx

--- app/pricing/page.jsx
+++ app/pricing/page.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import PaymentsAPI from "../components/payment";
+import React from "react"; 
 
 function Pricing() {
   return (
-    <>
-    <PaymentsAPI />
+    <> 
       <section className="bg-white">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
@@ -42,7 +40,22 @@ function Pricing() {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Access to AI based presentation critisizer</span>
+                  <span>Access to an AI-powered presentation evaluator</span> 
+                </li>
+                <li className="flex items-center space-x-3">
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>Speech Clarity Analysis</span> 
                 </li>
                 <li className="flex items-center space-x-3">
                   <svg
@@ -107,7 +120,7 @@ function Pricing() {
                 Get started
               </a>
             </div>
-          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow ">
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-purple-700  shadow ">
               <h3 className="mb-4 text-2xl font-semibold">Yearly</h3>
               <p className="font-light text-gray-500 sm:text-lg ">
                 Best for large scale uses and frequent customers.
@@ -131,7 +144,7 @@ function Pricing() {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Access to AI based presentation critisizer</span>
+                  <span>Access to AI based presentation criticizer</span>
                 </li>
                 <li className="flex items-center space-x-3">
                   <svg
@@ -146,7 +159,22 @@ function Pricing() {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>24x7 customer services</span>
+                  <span>Speech Clarity Analysis</span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-green-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>24x7 Customer Services</span>
                 </li>
                 <li className="flex items-center space-x-3">
                   <svg
